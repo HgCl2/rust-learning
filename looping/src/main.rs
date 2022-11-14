35dea440f800cfb982f6ef05ead5089f1d2b17cc
Almas Serik <s.almas_99@mail.ru>
2022-11-14 23:18:04 +0600

    Fixing println argument

--- looping/src/main.rs
+++ looping/src/main.rs
@@ -5,7 +5,7 @@ fn main() {
     let right: &str = "The letter e";
 
     loop {
-        println!("Riddle: I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?");
+        println!("I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?");
 
         let mut answer = String::new();
 
