d3372ea757f11e6ea8518b2c260ea4787fbb608c
Almas Serik <s.almas_99@mail.ru>
2022-11-15 18:38:20 +0600

    Example of using constants

---
+++ constants/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    // declaration using const keyword
+    // type of value must be annotated
+    // constants may set only constant expression
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+
+    println!("How many seconds in 3 hour? {THREE_HOURS_IN_SECONDS}");
+}
