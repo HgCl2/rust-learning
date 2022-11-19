5d59fe57106ea9b5ca28f9d4867e5dc77f9fe5f6
Almas Serik <s.almas_99@mail.ru>
2022-11-19 18:38:00 +0600

    quest 3 task 3: changing type of arg in sum function

--- arrays/src/lib.rs
+++ arrays/src/lib.rs
@@ -1,4 +1,4 @@
-pub fn sum(a: [i32; 32]) -> i32 {
+pub fn sum(a: &[i32]) -> i32 {
     let mut res: i32 = 0;
 
     for num in a{
@@ -18,7 +18,19 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = summ(thirtytwo_tens());
+        let result = sum(&thirtytwo_tens());
         assert_eq!(result, 320);
+    }
+
+    #[test]
+    fn case2() {
+        let a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        let a1: Vec<i32> = (1..11).collect();
+        let b = [5; 10];
+        assert_eq!(sum(&a), 55);
+        assert_eq!(sum(&a1), 55);
+        assert_eq!(sum(&b), 50);
+
+
     }
 }
