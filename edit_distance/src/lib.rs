8acea5183d4dddb6f4b940d0579bec3cd4e63ada
Almas Serik <s.almas_99@mail.ru>
2022-11-20 01:43:44 +0600

    q3 t5 changing case of dp variable

--- edit_distance/src/lib.rs
+++ edit_distance/src/lib.rs
@@ -2,14 +2,14 @@ pub fn edit_distance(source: &str, target: &str) -> usize {
     let len_source = source.chars().count();
     let len_target = target.chars().count();
 
-    let mut DP: Vec<Vec<usize>> = vec![vec![0 as usize; len_target + 1]; len_source + 1];
+    let mut dp: Vec<Vec<usize>> = vec![vec![0 as usize; len_target + 1]; len_source + 1];
 
-    for i in 1..len_source {
-        DP[i][0] = i;
+    for i in 1..=len_source {
+        dp[i][0] = i;
     }
 
-    for j in 1..len_target {
-        DP[0][j] = j;
+    for j in 1..=len_target {
+        dp[0][j] = j;
     }
 
     let mut substitution_cost: usize;
@@ -22,13 +22,13 @@ pub fn edit_distance(source: &str, target: &str) -> usize {
                 substitution_cost = 1;
             }
 
-            DP[i][j] = std::cmp::min(DP[i - 1][j] + 1,
-                std::cmp::min(DP[i][j - 1] + 1, DP[i - 1][j - 1] + substitution_cost)
+            dp[i][j] = std::cmp::min(dp[i - 1][j] + 1,
+                std::cmp::min(dp[i][j - 1] + 1, dp[i - 1][j - 1] + substitution_cost)
             );
         }
     }
 
-    return DP[len_source][len_target];
+    return dp[len_source][len_target];
 }
 
 #[cfg(test)]
