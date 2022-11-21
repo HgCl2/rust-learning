344a11b8b5c57909921a744fbee31c108968cb95
Almas Serik <s.almas_99@mail.ru>
2022-11-21 19:03:12 +0600

    quest4 task 1

---
+++ unwrap_or_expect/src/lib.rs
@@ -0,0 +1,45 @@
+pub enum Security {
+    Unknown,
+    High,
+    Medium,
+    Low,
+    BlockServer,
+}
+
+pub fn msg(x: String) -> String {
+    let mut res: String = String::new();
+    res.push_str("Not found: ");
+    res.push_str(&x);
+
+    return res.clone();
+}
+
+pub fn fetch_data(server: Result<String, String>, security_level: Security) -> String {
+    match security_level {
+        Security::Unknown => Err("".to_string()).unwrap(),
+        Security::High => Err("".to_string()).expect("ERROR: program stops"),
+        Security::Medium => server.unwrap_or("WARNING: check the server".to_string()),
+        Security::Low => server.unwrap_or_else(msg),
+        Security::BlockServer => server.unwrap_err(),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn medium() {
+        let result = fetch_data(Ok("server1.com".to_string()), Security::Medium);
+        assert_eq!(result, "server1.com");
+        let result = fetch_data(Err(String::new()), Security::Medium);
+        assert_eq!(result, "WARNING: check the server"); 
+    }
+
+    #[test]
+    fn low() {
+        let result = fetch_data(Err("server2.com".to_string()), Security::Low);
+        assert_eq!(result, "Not found: server2.com");
+    }
+
+}
