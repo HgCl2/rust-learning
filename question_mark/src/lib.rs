7513d573046a4062c1c05bad7787076d89e91a18
Almas Serik <s.almas_99@mail.ru>
2022-11-21 21:27:32 +0600

    q4 t5: changing access modifiers

--- question_mark/src/lib.rs
+++ question_mark/src/lib.rs
@@ -1,17 +1,17 @@
 pub struct One {
-    first_layer: Option<Two>
+    pub first_layer: Option<Two>
 }
 #[derive(Clone, Copy)]
 pub struct Two {
-    second_layer: Option<Three>
+    pub second_layer: Option<Three>
 }
 #[derive(Clone, Copy)]
 pub struct Three {
-    third_layer: Option<Four>
+    pub third_layer: Option<Four>
 }
 #[derive(Clone, Copy)]
 pub struct Four {
-    fourth_layer: Option<u16>
+    pub fourth_layer: Option<u16>
 }
 
 impl One {
