c4ce5b5cb7c00338732cb21e7a64e5d7180a9eb3
Almas Serik <s.almas_99@mail.ru>
2022-11-21 21:24:27 +0600

    quest4 task5

---
+++ question_mark/src/lib.rs
@@ -0,0 +1,39 @@
+pub struct One {
+    first_layer: Option<Two>
+}
+#[derive(Clone, Copy)]
+pub struct Two {
+    second_layer: Option<Three>
+}
+#[derive(Clone, Copy)]
+pub struct Three {
+    third_layer: Option<Four>
+}
+#[derive(Clone, Copy)]
+pub struct Four {
+    fourth_layer: Option<u16>
+}
+
+impl One {
+    pub fn get_fourth_layer(&self) -> Option<u16> {
+        return self.first_layer?.second_layer?.third_layer?.fourth_layer;
+    }
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let a = One {
+            first_layer : Some(Two {
+                second_layer: Some(Three {
+                    third_layer: Some(Four {
+                        fourth_layer: Some(1000)
+                    })
+                })
+            })
+        };
+        assert_eq!(a.get_fourth_layer(), Some(1000));
+    }
+}
