028f8323f7de83867baefc6b61c5cf52ae24cb66
Almas Serik <s.almas_99@mail.ru>
2022-11-23 19:26:11 +0600

    quest5 task1

---
+++ middle_day/src/lib.rs
@@ -0,0 +1,23 @@
+use chrono::Datelike;
+pub use chrono::{NaiveDate};
+pub use chrono::Weekday as wd;
+
+pub fn middle_day(year: usize) -> Option<wd> {
+    let middle_day_odd_year = 365 / 2 + 1;
+    if year % 4 != 0{
+        let date = NaiveDate::from_yo_opt(year as i32, middle_day_odd_year).unwrap();
+        Some(date.weekday())
+    } else {
+        None
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(middle_day(1022).unwrap(), wd::Tue);
+    }
+}
