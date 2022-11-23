3cd888c92c2345b45cd197398bb3736a90c8f050
Almas Serik <s.almas_99@mail.ru>
2022-11-24 03:00:09 +0600

    q5 t5: another attempt

--- expected_variable/src/lib.rs
+++ expected_variable/src/lib.rs
@@ -41,9 +41,8 @@ pub fn expected_variable(target_str: &str, expected_str: &str) -> Option<String>
 
     if target_str == expected_str{
         return Some("100%".to_string());
-    }else if (!target.is_camel_lowercase() && target != target.to_snake()) ||
-        target.contains(|c: char| c != '_' && c == ' ' && c.is_ascii_punctuation()) ||
-        target_str == "" || expected_str == ""{
+    }else if (!target.is_camel_lowercase() && target_str.to_camel().to_snake() != target) ||
+        target_str.contains(['-', ' ']) || target_str == "" || expected_str == ""{
         return  None;
     }
 
