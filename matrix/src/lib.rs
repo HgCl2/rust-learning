be695abef103a3b2822dce4628a84866093f1c02
Almas Serik <s.almas_99@mail.ru>
2022-11-26 03:57:35 +0600

    template for q6 t5

---
+++ matrix/src/lib.rs
@@ -0,0 +1,100 @@
+use std::ops::{Add, Sub, Div, Mul};
+
+pub trait Scalar: Add + Sub + Div + Mul + Sized{
+	type Item;
+	fn zero() -> Self::Item;
+	fn one() -> Self::Item;
+}
+
+impl Scalar for f64 {
+    type Item = f64;
+    fn zero() -> Self::Item {
+        return 0.0;
+    }
+
+    fn one() -> Self::Item {
+        return 1.0;
+    }
+}
+
+impl Scalar for f32 {
+    type Item = f32;
+    fn zero() -> Self::Item {
+        return 0.0;
+    }
+
+    fn one() -> Self::Item {
+        return 1.0;
+    }
+}
+
+impl Scalar for i64 {
+    type Item = i64;
+    fn zero() -> Self::Item {
+        return 0;
+    }
+
+    fn one() -> Self::Item {
+        return 1;
+    }
+}
+
+impl Scalar for i32 {
+    type Item = i32;
+    fn zero() -> Self::Item {
+        return 0;
+    }
+
+    fn one() -> Self::Item {
+        return 1;
+    }
+}
+
+impl Scalar for u64 {
+    type Item = u64;
+    fn zero() -> Self::Item {
+        return 0;
+    }
+
+    fn one() -> Self::Item {
+        return 1;
+    }
+}
+
+impl Scalar for u32 {
+    type Item = u32;
+    fn zero() -> Self::Item {
+        return 0;
+    }
+
+    fn one() -> Self::Item {
+        return 1;
+    }
+}
+
+#[derive(Clone)]
+pub struct Matrix<T>(pub Vec<Vec<T>>);
+
+impl <T: Scalar<Item = T> + std::clone::Clone> Matrix<T> {
+	pub fn new() -> Matrix<T> {
+        return Matrix(vec![]);
+	}
+
+	pub fn zero(row: usize, col: usize) -> Matrix<T> {
+        return Matrix(vec![vec![Scalar::zero(); col]; row]);
+	}
+
+	pub fn identity(n: usize) -> Matrix<T> {
+        let mut result = Vec<T>::new();
+	}
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+
+    }
+}
