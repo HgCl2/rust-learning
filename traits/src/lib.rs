9fdad4afad022fdab4ab6f1f19c03d18490906f3
Almas Serik <s.almas_99@mail.ru>
2022-11-25 21:55:11 +0600

    Template q6 task2

---
+++ traits/src/lib.rs
@@ -0,0 +1,50 @@
+pub fn (left: usize, right: usize) -> usize {
+    
+}
+
+#[derive(Debug)]
+pub struct Player {
+    pub name: f64,
+    pub strength: f64,
+    pub score: i32,
+    pub money: i32,
+    pub weapons: Vec<String>,
+}
+
+pub struct Fruit {
+    pub weight_in_kg: f64,
+}
+
+pub struct Meat {
+	pub weight_in_kg: f64,
+	pub fat_content: f64,
+}
+
+impl Player {
+	fn eat(&mut self, food: T) {
+		self.strength += food.gives();
+	}
+}
+
+pub trait Food {
+	fn gives(&self) -> f64;
+}
+
+impl Food for Fruit {
+    
+}
+
+impl Food for Meat {
+
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
