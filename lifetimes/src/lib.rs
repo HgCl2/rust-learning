5a8795bcbad467516a124df9776ba4fd45896df8
Almas Serik <s.almas_99@mail.ru>
2022-11-26 02:53:18 +0600

    q6 t3: lifetimes

---
+++ lifetimes/src/lib.rs
@@ -0,0 +1,23 @@
+
+
+#[derive(Debug)]
+pub struct Person<'a>{
+	pub name: &'a str,
+	pub age: u8,
+}
+
+impl Person<'_> {
+	pub fn new<'a>(name: &'a str) -> Person {
+        Person { name: name, age: 0 }
+	}
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        
+    }
+}
