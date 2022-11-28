e9389652ca5cac63b9aeae9f89bfa1999be10e20
Almas Serik <s.almas_99@mail.ru>
2022-11-28 20:35:45 +0600

    q7 t4

---
+++ how_many_references/src/lib.rs
@@ -0,0 +1,36 @@
+pub use std::rc::Rc;
+
+pub struct Node {
+    pub ref_list: Vec<Rc<String>>,
+}
+
+impl Node {
+    pub fn new(ref_list: Vec<Rc<String>>) -> Node {
+        Node { ref_list: ref_list }
+    }
+    pub fn add_element(&mut self, element: Rc<String>) {
+        self.ref_list.push(element);
+    }
+    pub fn rm_all_ref(&mut self, element: Rc<String>) {
+        let twin = self.ref_list.clone();
+        for ind in (0..twin.len()).rev(){
+            if twin[ind] == element{
+                self.ref_list.remove(ind);
+            }
+        }
+    }
+}
+
+pub fn how_many_references(ref_list: &Rc<String>) -> usize {
+    return Rc::strong_count(ref_list);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        
+    }
+}
