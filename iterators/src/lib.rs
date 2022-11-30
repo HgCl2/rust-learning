43912e598aa6724421d95cd15dc72e4c42956862
Almas Serik <s.almas_99@mail.ru>
2022-11-30 22:48:58 +0600

    q8 t7: new view to task

--- iterators/src/lib.rs
+++ iterators/src/lib.rs
@@ -6,18 +6,12 @@ pub struct Collatz {
 impl Iterator for Collatz {
     type Item = Collatz;
     fn next(&mut self) -> Option<Self::Item> {
-        if self.v == 0 || self.v == 1{
+        if self.v == u64::MAX{
             return None;
-        }
-        if self.v % 2 == 0{
-            self.v = self.v / 2;
-            return Some(*self);
         }else {
-            self.v = self.v * 3 + 1;
+            self.v += 1;
             return Some(*self);
         }
-
-       
     }
     
 }
@@ -37,7 +31,11 @@ pub fn collatz(n: u64) -> usize {
     let mut collatz_strt = Collatz::new(n);
     while collatz_strt.v != 1 {
         counter += 1;
-        collatz_strt.next();
+        if collatz_strt.v % 2 == 0{
+            collatz_strt.v = collatz_strt.v / 2;
+        }else {
+            collatz_strt.v = collatz_strt.v * 3 + 1;
+        }
     }
 
     return counter;
